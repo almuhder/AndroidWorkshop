ff28feef69b237e471c591828c3d3dc3d9cf0ab3
Majd Eddin <almuhder@gmail.com>
2017-05-08 15:18:41 +0300

    +Spinner +RadioGroup

--- app/src/main/java/org/rbk/androidworkshop/RegisterUser.java
+++ app/src/main/java/org/rbk/androidworkshop/RegisterUser.java
@@ -3,20 +3,32 @@ package org.rbk.androidworkshop;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class RegisterUser extends AppCompatActivity {
 
 
+
+
     // 1. First define objects from each class you want to use from the UI (XML)
     EditText etName, etPhone, etEmail, etPassword;
     RadioButton rbMale, rbFemale;
-    ListView lvCity;
+    RadioGroup rgGender;
+    Spinner spCity;
     CheckBox cbAgree;
+    Button btnRegister;
+
+    // Define a list of cities
+    String[] cities = {"Amman", "Ibrid", "Salet", "Aqaba", "Maan"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,18 +43,30 @@ public class RegisterUser extends AppCompatActivity {
         rbFemale = (RadioButton) findViewById(R.id.rbFemale);
         rbMale = (RadioButton) findViewById(R.id.rbMale);
         cbAgree = (CheckBox) findViewById(R.id.cbAgree);
-        lvCity = (ListView) findViewById(R.id.lvCity);
+        spCity = (Spinner) findViewById(R.id.spCity);
+        rgGender = (RadioGroup) findViewById(R.id.rgGender);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
 
         // 3. Write java code to control the UI from Java
         // now you can set and get the values of the UI
 
+        // link between Spinner and array
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_spinner_dropdown_item , cities);
+        spCity.setAdapter(adapter);
+
+        // this will disable the button for validation
+//        btnRegister.setEnabled(false);
+
     }
 
     public void showUserInfo(View v){
-        Toast.makeText(this, "Name: " + etName.getText()
+        Toast.makeText(this, // Context : this, Current class , Where the Toast msg supposed to be shown
+                "Name: " + etName.getText() // the message
                 + " Phone " + etPhone.getText()
                 + " Email " + etEmail.getText()
-                , Toast.LENGTH_SHORT).show();
+                , Toast.LENGTH_SHORT) // The duration : Toast.LENGTH_LONG
+                .show();
 
     }
 }
